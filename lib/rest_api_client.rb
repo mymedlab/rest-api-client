da3c68d69927686b90a5fd207297485995f3800a
Casey Provost <provost.design@gmail.com>
2016-11-27 13:11:27 -0500

    add configuration file

--- lib/rest_api_client.rb
+++ lib/rest_api_client.rb
@@ -1,4 +1,5 @@
 require "rest_api_client/version"
+require "rest_api_client/configuration"
 require "rest_api_client/resource"
 require "rest_api_client/entity_attribute"
 require "rest_api_client/entity"
